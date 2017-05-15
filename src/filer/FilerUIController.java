c2678bfd27d48625c9ec3ba5cd6135b0f14ba9d7
Richard Craggs <rcraggs@gmail.com>
2017-05-15 20:13:23 +0100

    Completed first version

--- src/filer/FilerUIController.java
+++ src/filer/FilerUIController.java
@@ -1,19 +1,40 @@
 package filer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Properties;
 
 public class FilerUIController {
 
+    private Properties properties;
     private String inputFilePath;
+    private List<String> directories = null;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private ListView<String> directory_list;
 
     @FXML
     private Label input_file_path_label;
@@ -21,23 +42,124 @@ public class FilerUIController {
     @FXML
     private TextField new_file_name_text_box;
 
-    public FilerUIController() { }
+    private String searchTerm = "";
 
-    public void setInputFilePath(String path) {
-        this.input_file_path_label.setText(path);
-        this.inputFilePath = path;
+    public FilerUIController() {
+
+        properties = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("SSFiler.properties");
+            properties.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // Store the list of directories
+        try {
+            directories = Files.readAllLines(Paths.get(properties.getProperty("directory_list")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+
+        VBox.setVgrow(directory_list, Priority.ALWAYS);
+
+        this.new_file_name_text_box.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("MMM-yyyy")) + ".pdf");
+        this.new_file_name_text_box.setOnAction(event -> {
+            this.moveFile();
+        });
+
+        FilteredList<String> filteredData = new FilteredList<>(
+                FXCollections.observableArrayList(this.directories), p -> true);
+
+        directory_list.setItems(filteredData);
+
+        directory_list.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+
+            if (event.getCode() == KeyCode.ENTER){
+                this.moveFile();
+            }
+
+            if (event.getCode() != KeyCode.DOWN && event.getCode() != KeyCode.UP) {
+
+                if (event.getCode() == KeyCode.BACK_SPACE) {
+                    this.searchTerm = "";
+                }else{
+                    this.searchTerm += event.getText().trim();
+                }
+
+                filteredData.setPredicate(directory ->
+                        this.stringContainsLettersInOrder(directory.toLowerCase(),
+                                this.searchTerm.toLowerCase()));
+
+                // If the data has changed set the first item as selected
+                if (filteredData.size() > 0){
+                    this.directory_list.getSelectionModel().select(0);
+                }
+            }
+        });
+    }
+
+    private boolean stringContainsLettersInOrder(String haystack, String needle) {
+
+        if (needle.equals(haystack)){
+            return true;
+        }
+        else if (needle.length() == 0){
+            return true;
+        }else{
+            int nextCharAt = haystack.indexOf(needle.charAt(0));
+            if (nextCharAt > -1){
+                return (stringContainsLettersInOrder(haystack.substring(nextCharAt+1), needle.substring(1)));
+            }else{
+                return false;
+            }
+        }
+    }
+
+    void setInputFilePath(String path) {
+
+        if (Files.exists(Paths.get(path))){
+            this.input_file_path_label.setText(path);
+            this.inputFilePath = path;
+        } else {
+            this.input_file_path_label.setText("Doesn't exist: " + path);
+        }
     }
 
     @FXML
     private void fileButtonAction(ActionEvent event) {
+        moveFile();
+    }
+
+    private void moveFile() {
 
         Path sourcePath = Paths.get(this.inputFilePath);
-        Path destPath = Paths.get(sourcePath.getParent().toString(), this.new_file_name_text_box.getText());
+        Path destPath = Paths.get(
+                this.properties.getProperty("root"),
+                this.directory_list.getSelectionModel().getSelectedItem(),
+                this.new_file_name_text_box.getText());
 
         try {
             Files.move(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            System.exit(0);
         }
     }
 }
--- src/filer/Main.java
+++ src/filer/Main.java
@@ -1,9 +1,14 @@
 package filer;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -24,7 +29,14 @@ public class Main extends Application {
 
 
         primaryStage.setTitle("ScanSnap Filer");
-        primaryStage.setScene(new Scene(root, 400, 275));
+        primaryStage.setScene(new Scene(root, 400, 1000));
+
+        primaryStage.getScene().addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+            if (event.getCode() == KeyCode.ESCAPE){
+                System.exit(0);
+            }
+        });
+
         primaryStage.show();
     }
 
